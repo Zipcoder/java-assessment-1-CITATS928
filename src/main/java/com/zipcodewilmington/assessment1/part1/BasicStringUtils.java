ecbee18981e025585898d3cfe52b5ddc9c4d1c18
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-07-08 10:14:01 -0400

    Finished IntegerArrayUtils

--- src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
@@ -1,5 +1,10 @@
 package com.zipcodewilmington.assessment1.part1;
 
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.Locale;
+
+
 /**
  * Created by leon on 2/16/18.
  */
@@ -9,7 +14,21 @@ public class BasicStringUtils {
      * @return string with identical content, and the first character capitalized
      */
     public static String camelCase(String str) {
-        return null;
+//        String[] arr = str.split(" ");
+//        System.out.println(arr[0].toUpperCase(Locale.ROOT));
+//        String temp=arr[0].toUpperCase(Locale.ROOT);
+//        arr[0]=temp;
+//        String result="";
+//        for(int i=0;i<arr.length;i++){
+//            result +=arr[i]+" ";
+//        }
+        String first = str.substring(1);
+        char result = str.toUpperCase().charAt(0);
+        //System.out.println(result);
+        //System.out.println(first);
+        String result1 = result+first;
+
+        return result1;
     }
 
     /**
@@ -17,7 +36,10 @@ public class BasicStringUtils {
      * @return string with identical contents, in the reverse order
      */
     public static String reverse(String str) {
-        return null;
+        StringBuilder stringBuilder=new StringBuilder(str);
+        String result = stringBuilder.reverse().toString();
+
+        return result;
     }
 
     /**
@@ -25,7 +47,12 @@ public class BasicStringUtils {
      * @return string with identical contents, in reverse order, with first character capitalized
      */
     public static String reverseThenCamelCase(String str) {
-        return null;
+        String result = reverse(str);
+        result=camelCase(result);
+
+
+
+        return result;
     }
 
 
@@ -34,7 +61,7 @@ public class BasicStringUtils {
      * @return string with identical contents excluding first and last character
      */
     public static String removeFirstAndLastCharacter(String str) {
-        return null;
+        return str.substring(1,str.length()-1);
     }
 
     /**
@@ -42,6 +69,16 @@ public class BasicStringUtils {
      * @return string with identical characters, each with opposite casing
      */
     public static String invertCasing(String str) {
-        return null;
+        char[] cha=str.toCharArray();
+        for(int i=0;i<cha.length;i++){
+            char c = cha[i];
+            if(Character.isUpperCase(c)){
+                cha[i]=Character.toLowerCase(c);
+            }else if(Character.isLowerCase(c)){
+                cha[i]=Character.toUpperCase(c);
+            }
+        }
+        //String result = new String(cha);
+        return new String(cha);
     }
 }
